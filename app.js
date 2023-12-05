fabc596c3e4d40f21e5c82469658a52289976b87
fuadfatali1 <fuad0fataliyev0@outlook.com>
2023-12-05 21:24:28 +0400

    initial version

--- app.js
+++ app.js
@@ -1,35 +1,137 @@
 document.addEventListener('DOMContentLoaded', function () {
-    let products = document.querySelector('.products');
+    const productsContainer = document.querySelector('.products');
+    const searchInput = document.getElementById('search');
+    const categorySelect = document.getElementById('category');
+    const listPage = document.querySelector('.listPage');
+    let productsData = []; // To store the fetched products data
+    let currentPage = 1;
+    const limitPerPage = 10;
 
     async function fetchProducts(url) {
         try {
-            let data = await fetch(url);
-            if (!data.ok) {
-                throw new Error(`Error ${data.status}: ${data.statusText}`);
-            }
-
-            let response = await data.json();
-            let productsArray = response.products;
-
-            // Iterate through each product and create HTML elements
-            productsArray.forEach(product => {
-                products.innerHTML += `
-                    <div class="product">
-                        <img src="${product.thumbnail}" alt="${product.title}" class="product-img">
-                        <h2 class="product-title">${product.title}</h2>
-                        <h4 class="product-category">${product.category}</h4>
-                        <p class="product-description">${product.description}</p>
-                        <div class="product-price-container">
-                            <h3 class="product-price">${product.price}</h3>
-                            <a href="#" data-productId="${product.id}" class="more-detailed">More Details</a>
-                        </div>
-                    </div>
-                `;
-            });
+            const response = await fetch(url);
+            const data = await response.json();
+            productsData = data.products;
+            displayProducts(productsData); // Display all products initially
+            populateCategoriesDropdown(productsData); // Populate categories in the dropdown
+            showPage(productsData, currentPage);
+            setupPagination(productsData);
         } catch (error) {
             console.error('Error fetching products:', error);
         }
     }
 
-    fetchProducts('https://dummyjson.com/products');
+    function displayProducts(products) {
+        productsContainer.innerHTML = ''; // Clear previous product display
+        products.forEach(product => {
+            let final_value = (product.price - (product.price * product.discountPercentage / 100)).toFixed(2);
+            let stk = product.stock;
+            // Create HTML for each product
+            productsContainer.innerHTML += `
+                <div class="product">
+                    <img src="${product.thumbnail}" alt="" class="product-img">
+                    <div class="product-content">
+                    <h2 class="product-title">${product.title}</h2>
+                    <h2 class="product-brand">${product.brand}</h2>
+                    <h4 class="product-category">${product.category}</h4>
+                    <p class="product-description">${product.description}</p>
+                    <div class="product-price-container">
+                        <h3 class="product-price">$${product.price}</h3>
+                        <h3 class="product-discount-price">${product.discountPercentage}%</h3>
+                        <h3 class="product-final-price">$${final_value}</h3>
+                        <h3 class="product-rating">${product.rating} (out of 5)</h3>
+                        <h3 class="product-stock" style="color: ${stk < 50 ? 'red' : 'inherit'};">Stock: ${stk}</h3>
+                        <a href="productDetails.html?id=${product.id}" data-productId="" class="more-detail">More detail</a>
+                        <hr>
+                    </div
+                    </div>
+                </div>`;
+        });
+    }
+
+    function populateCategoriesDropdown(products) {
+        // Get unique categories from products
+        const categories = [...new Set(products.map(product => product.category))];
+        // Populate select box with categories
+        categories.forEach(category => {
+            const option = document.createElement('option');
+            option.value = category;
+            option.textContent = category;
+            categorySelect.appendChild(option);
+        });
+    }
+
+    function showPage(products, page) {
+        const startIndex = (page - 1) * limitPerPage;
+        const endIndex = startIndex + limitPerPage;
+        const paginatedProducts = products.slice(startIndex, endIndex);
+        displayProducts(paginatedProducts);
+    }
+    function setupPagination(products) {
+        listPage.innerHTML = '';
+        const pageCount = Math.ceil(products.length / limitPerPage);
+        for (let i = 1; i <= pageCount; i++) {
+            const li = document.createElement('li');
+            li.textContent = i;
+            li.addEventListener('click', function (event) {
+                event.preventDefault();
+                currentPage = i;
+                showPage(products, currentPage);
+                updatePagination();
+                window.scrollTo({ top: 0, behavior: 'smooth' }); // Scroll to the top of the page
+            });
+            listPage.appendChild(li);
+        }
+        updatePagination();
+    }
+    
+    function updatePagination() {
+        const pages = document.querySelectorAll('.listPage li');
+        pages.forEach((page, index) => {
+            if (index + 1 === currentPage) {
+                page.classList.add('active');
+            } else {
+                page.classList.remove('active');
+            }
+        });
+    }
+
+    // Fetch products when the page loads
+
+
+    fetchProducts('https://dummyjson.com/products?limit=100');
+    function filterProducts(keyword, selectedCategory) {
+        const filteredProducts = productsData.filter(product => {
+            const matchesKeyword = (
+                product.title.toLowerCase().includes(keyword.toLowerCase()) ||
+                product.description.toLowerCase().includes(keyword.toLowerCase())
+            );
+
+            const matchesCategory = (selectedCategory === 'all' || product.category === selectedCategory);
+
+            return matchesKeyword && matchesCategory;
+        });
+        return filteredProducts;
+    }
+
+    // Search input event listener
+ searchInput.addEventListener('input', function (event) {
+        const keyword = event.target.value.trim();
+        const selectedCategory = categorySelect.value;
+        const filteredProducts = filterProducts(keyword, selectedCategory);
+        currentPage = 1;
+        showPage(filteredProducts, currentPage);
+        setupPagination(filteredProducts);
+    });
+
+    categorySelect.addEventListener('change', function (event) {
+        const selectedCategory = event.target.value;
+        const keyword = searchInput.value.trim();
+        const filteredProducts = filterProducts(keyword, selectedCategory);
+        currentPage = 1;
+        showPage(filteredProducts, currentPage);
+        setupPagination(filteredProducts);
+    });
 });
+
+
---
+++ details.js
@@ -0,0 +1,75 @@
+document.addEventListener('DOMContentLoaded', async function () {
+    const urlParams = new URLSearchParams(window.location.search);
+    const productId = urlParams.get('id');
+
+    try {
+        const productDetails = await fetch(`https://dummyjson.com/product/${productId}`);
+        const product = await productDetails.json();
+
+        const productDetailsContainer = document.querySelector('.product-details-container');
+
+        const finalValue = (product.price - (product.price * product.discountPercentage / 100)).toFixed(2);
+        const stock = product.stock;
+
+        productDetailsContainer.innerHTML = `
+        <div class="product-details">
+            <h1>Product Details</h1>
+            <div class="product-details-container">
+                ${product.images.map(image => `<img src="${image}" alt="${product.title}" class="product-img">`).join('')}
+                <h2 class="product-title">${product.title}</h2>
+                <h3 class="product-price">$${product.price}</h3>
+                <h3 class="product-final-price">$${finalValue}</h3>
+                <p class="product-description">${product.description}</p>
+                <!-- Include other product details you want to display -->
+                <h3 class="product-stock" style="color: ${stock < 50 ? 'red' : 'inherit'};">Stock: ${stock}</h3>
+            </div>
+        </div>`;
+
+        const currentProductCategory = product.category; // Replace this with the actual property name
+
+        // Fetch products with the same category
+        const similarProductsResponse = await fetch(`https://dummyjson.com/products/category/${encodeURIComponent(currentProductCategory)}`);
+        const similarProductsData = await similarProductsResponse.json();
+        const similarProductsArray = similarProductsData.products;
+
+        const similarProductsContainer = document.querySelector('.similar-products-container');
+        let count = 0;
+
+        // Display similar products in the HTML
+        similarProductsArray.forEach(similarProduct => {
+            if (similarProduct.id !== product.id) {
+                // Create HTML elements for each similar product with a link to its details page
+                const similarProductHTML = `
+                    <div class="similar-product">
+                        <a href="productDetails.html?id=${similarProduct.id}">
+                            <img src="${similarProduct.thumbnail}" alt="${similarProduct.title}" class="similar-product-img">
+                            <h2 class="product-title">${similarProduct.title}</h2>
+                            <h3 class="product-price">$${similarProduct.price}</h3>
+                            <h3 class="product-final-price">$${finalValue}</h3>
+                            <p class="product-description">${similarProduct.description}</p>
+                            <!-- Include other product details you want to display -->
+                            <h3 class="product-stock" style="color: ${similarProduct.stock < 50 ? 'red' : 'inherit'};">Stock: ${similarProduct.stock}</h3>
+                            <hr>
+                        </a>
+                    </div>
+                `;
+
+                similarProductsContainer.innerHTML += similarProductHTML;
+                count++;
+            }
+        });
+
+        if (count === 0) {
+            const similarProductHTML = `
+                <div class="similar-product">
+                    <h1>There are not any similar objects</h1>
+                </div>
+            `;
+            similarProductsContainer.innerHTML += similarProductHTML;
+        }
+
+        
+    } catch (error) {
+        console.error('Error fetching product details:', error);
+    }
+});
